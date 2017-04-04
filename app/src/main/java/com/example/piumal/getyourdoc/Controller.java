9bfa1e25203a54ef441ee947526e120cde94d8c5
Piumal Kulasekara <piumal.2015309@iit.ac.lk>
2017-04-04 23:24:53 +0530

    Main Activity XML completed

---
+++ app/src/main/java/com/example/piumal/getyourdoc/Controller.java
@@ -0,0 +1,7 @@
+package com.example.piumal.getyourdoc;
+
+/**
+ * Created by piumal on 4/4/17.
+ */
+public class Controller {
+}
--- app/src/main/java/com/example/piumal/getyourdoc/MainActivity.java
+++ app/src/main/java/com/example/piumal/getyourdoc/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.piumal.getyourdoc;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.widget.Button;
+import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private AppointmentsDate appointments;
+    private static String[] FROM = {DETAILS};
+    private long date;
+    private String data;
+    private String title;
+    private String time;
+    private String details = "";
+    private EditText titleET;
+    private EditText timeET;
+    private EditText detailsET;
+    private Controller controller;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
